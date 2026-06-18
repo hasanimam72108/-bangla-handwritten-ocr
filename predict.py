8b0fdfe2e56aae66d896dec30132f0a98944ca69
Jawadur Rafid <jawadurrafidrafid@gmailcom>
2026-06-19 02:38:24 +0600

    feat: set freeze_encoder to True across prediction, training, and evaluation pipelines

--- predict.py
+++ predict.py
@@ -50,7 +50,7 @@ def main():
         tokenizer=tokenizer,
         max_length=config["model"]["max_length"],
         dropout=config["model"]["dropout"],
-        freeze_encoder=False,
+        freeze_encoder=True,
     )
     model.to(device)
 
--- train.py
+++ train.py
@@ -105,7 +105,7 @@ def main():
         tokenizer=tokenizer,
         max_length=config["model"]["max_length"],
         dropout=config["model"]["dropout"],
-        freeze_encoder=False,
+        freeze_encoder=True,
     )
     model.to(device)
 
